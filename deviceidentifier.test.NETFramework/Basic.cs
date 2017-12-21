30a19c2a88ae9a86762ada693e601bcdc3201988
David Higgins <david@reincubate.com>
2017-12-21 13:39:00 +0000

    fixes, test project, reorganisation

---
+++ deviceidentifier.test.NETFramework/Basic.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Reincubate.DeviceIdentifier;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace deviceidentifier.test.NETFramework
+{
+    [TestFixture]
+    public class Basic
+    {
+        [Test]
+        public void DoesItRun()
+        {
+            // FIXME: START PATCH
+            // We want to get this response data so that we can build out a
+            // new "metadata" device pane. However, we can't be confident that
+            // the underlying API call will be rapid or robust. Thus it should
+            // be asynchronous AND handled such that any failure does not
+            // interupt the running of the app.
+            var api = new Api("2b2471807be846bb824286f88f3adb02");
+
+            var identifiers = new Identifiers
+            (
+              _apple_model: null, // We don't have this yet
+              _apple_serial: "ABC123",
+              _apple_identifier: "ios1000",
+              _gsma_imei: "LOL",
+              _gsma_iccid: "1234",
+              _cdma_meid: "memememememe"
+            );
+
+            Response response = api.EnhanceMetadata(identifiers);
+            // FIXME: END SECTION
+
+            Assert.IsNotNull(response);
+
+        }
+    }
+}
---
+++ deviceidentifier/Api.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Reincubate.DeviceIdentifier.Util;
+
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Reincubate.DeviceIdentifier
+{
+    public class Identifiers
+    {
+        public Identifiers(String _apple_model = null,
+                            String _apple_serial = null, String _apple_identifier = null,
+                            String _gsma_imei = null, String _gsma_iccid = null,
+                            String _cdma_meid = null)
+        {
+            apple_model = _apple_model;
+            apple_serial = _apple_serial;
+            apple_identifier = _apple_identifier;
+            gsma_imei = _gsma_imei;
+            gsma_iccid = _gsma_iccid;
+            cdma_meid = _cdma_meid;
+        }
+
+        public String apple_model;
+        public String apple_serial;
+        public String apple_identifier;
+        public String gsma_imei;
+        public String gsma_iccid;
+        public String cdma_meid;
+    }
+
+    public class Api
+    {
+        class ApiAuthenticator : IAuthenticator
+        {
+            readonly String token;
+            public ApiAuthenticator(String token)
+            {
+                if (token == null)
+                    throw new MissingTokenException("Token not specified; must be provided to authenticate with the API");
+                this.token = token;
+            }
+            public void Authenticate(IRestClient client, IRestRequest request)
+            {
+                request.AddHeader("Authorization", String.Format("Token {0}", token));
+            }
+        }
+
+        readonly RestClient client;
+        public Api(String token)
+        {
+            client = new RestClient("https://di-api.reincubate.com/");
+            client.Authenticator = new ApiAuthenticator(token);
+        }
+
+        public IList<String> IdentifyIdentifier(String identifier)
+        {
+
+            var request = new RestRequest("identify-identifier/", Method.POST);
+
+            request.AddJsonBody(new
+            {
+                data = new
+                {
+                    identifiers = new
+                    {
+                        unknown = identifier
+                    }
+                }
+            });
+
+            var response = client.Execute(request);
+
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+                //  throw new Exception( "API call not successful, response code %s:\n%s" % ( response.status_code, response.content ) )
+            }
+
+            var json = JObject.Parse(response.Content);
+
+            IList<String> types = json.SelectToken(String.Format("identifiers.{0}", identifier)).Select(s => (String)s).ToList();
+
+            return types;
+        }
+
+        public Response EnhanceMetadata(Identifiers identifiers)
+        {
+            RestRequest request = new RestRequest("enhance-metadata/", Method.POST);
+
+            request.AddJsonBody(new
+            {
+                data = new
+                {
+                    identifiers = identifiers
+                },
+            });
+
+            var response = client.Execute(request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+                //  throw new Exception( "API call not successful, response code %s:\n%s" % ( response.status_code, response.content ) )
+            }
+
+            var deserialised = JsonConvert.DeserializeObject<Response>(response.Content);
+            return deserialised;
+        }
+    }
+}
